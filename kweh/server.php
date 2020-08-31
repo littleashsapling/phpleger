1c7e22b1de7a88fa1418dfa805f644dc8fb5c7dd
littleashsapling <59930398+littleashsapling@users.noreply.github.com>
2020-08-31 12:11:00 -0300

    error can't find bug jermaine tomorrow

--- kweh/server.php
+++ kweh/server.php
@@ -2,9 +2,40 @@
 
 define('GALLERY_DIR', 'gallery/'); //why is it angry, precious?
 
-require 'db.php'; //says to use include instead?
+require 'db.php'; //fixed
 
-//action dispatcher - if you build it they will come
+//action dispatcher - built
+
+$action = $_POST['action'] ?? false;
+
+switch ($action) {
+  case 'register':
+    register();
+    break;
+  case 'login':
+    login();
+    break;
+  case 'logout':
+    logout();
+    break;
+  case 'addKweh':
+    addPost();
+    break;
+  case 'deleteKweh':
+    deletePost();
+    break;
+  case 'uploadImg':
+    uploadFile();
+    break;
+  case 'deleteImg':
+    deleteUpload();
+    break;
+    case 'comment':
+      comment();
+    break;
+  default:
+    redirect("404.php");
+}
 
 //helpers
 function authError($msg = "Bad Credentials, Please try again!")
@@ -16,13 +47,95 @@ function authError($msg = "Bad Credentials, Please try again!")
 //all my functions
 function register(){
 //registers user
-}
-function login(){
-//logs in user
+$useremail = $_POST['useremail']?? false;
+  $username = $_POST['username'] ?? false;
+  $password = $_POST['password'] ?? false;
+
+
+  if (!$useremail || !$username || !$password) {
+    echo "Need email, username and password";
+    return;
 }
 
-function loginSuccess(){
+global $con;
+
+  $query = <<<QUERY
+  INSERT INTO user (email, username, password) VALUE (:uemail, :uname , :pass)
+  QUERY;
+
+  $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+  $stm = $con->prepare($query);
+
+  $stm->bindParam(:"uemail", $useremail);
+  $stm->bindParam(":uname", $username);
+  $stm->bindParam(":pword", $hashedPassword);
+
+  if ($stm->execute()) {
+    loginSuccess([
+      'id' => $con->lastInsertId(),
+      'username' => $username,
+      'admin' => false
+    ]);
+  } else {
+    redirect("index.php");
+    authError("Username taken");
+  }
+}
+
+
+function login(){
+//logs in user
+  $username = $_POST['username'] ?? false;
+  $password = $_POST['password'] ?? false;
+
+  if (!$username || !$password) {
+    echo "Need username and password";
+    return;
+  }
+
+  global $con;
+
+  $query = <<<QUERY
+  SELECT id, username, password 
+  FROM user 
+  WHERE username = :uname
+  QUERY;
+
+  $stm = $con->prepare($query);
+
+  $stm->bindParam(":uname", $username);
+
+  if ($stm->execute()) {
+    if ($user = $stm->fetch(PDO::FETCH_ASSOC)) {
+      // Found a user
+      if (password_verify($password, $user['password'])) {
+        // Login successful
+        loginSuccess($user);
+      } else {
+        redirect("index.php");
+        authError();
+      }
+    } else {
+      redirect("index.php");
+      authError();
+    }
+  } else {
+    // Something went wrong with the DB
+    redirect("index.php");
+    authError("Internal DB Error");
+  }
+
+}
+
+function loginSuccess($user){
 //login success
+  redirect("posts.php");
+
+  // session stuff
+  session_start();
+  $_SESSION['userId'] = $user['id'];
+  $_SESSION['username'] = $user['username'];
 }
 
 function logout(){
